3650de82c866e2b247832c957f779e96648e221a
Enes <enes.buke12@gmail.com>
2023-09-17 18:38:35 +0300

    Second Update to project.

--- src/Utility/Func.java
+++ src/Utility/Func.java
@@ -1,5 +1,9 @@
 package Utility;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
 public class Func {
 
     public static void Wait(int sn) {
@@ -11,6 +15,11 @@ public class Func {
     }
     public static int createRandomNumb(int boundary){
         return (int)(Math.random()*boundary);
+    }
+    public void hoverFunction(WebElement element){
+        Actions actions=new Actions(BaseDriverMethod.driver);
+        Action action = actions.moveToElement(element).build();
+        action.perform();
     }
 
 }
